0ccf88164bcd1c9b547d58100de00c0eeb70de9f
davidhardin <thisgoestodavid@gmail.com>
2024-01-11 16:52:46 -0700

    more signup

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -1,38 +1,7 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 const Practice = () => {
-  const [name, setName] = useState('')
-  const [other, setOther] = useState('fancy pants')
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    console.log('fuck you')
-    console.log(name)
-    setOther(name)
-  }
-
-  return (
-    <div className=''>
-      <form onSubmit={onSubmit}>
-        <div className=''>
-          <label className='form-label text-white'>Name</label>
-          <input
-            type='text'
-            className='form-control text-black'
-            id='name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-
-        <button type='submit' data-bs-dismiss='modal' className='bg-slate-600'>
-          Submit
-        </button>
-      </form>
-      <p className='text-white'>{other}</p>
-    </div>
-  )
+  return <></>
 }
 
 export default Practice
--- src/pages/SignUpList.jsx
+++ src/pages/SignUpList.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useQuery, gql } from '@apollo/client'
 
 const query = gql`
@@ -15,7 +16,38 @@ const SignUpList = () => {
   const { data } = useQuery(query)
   console.log(data)
 
-  return <div>SignUp List</div>
+  const [name, setName] = useState('')
+  const [other, setOther] = useState('fancy pants')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('fuck you')
+    console.log(name)
+    setOther(name)
+  }
+
+  return (
+    <div className=''>
+      <form onSubmit={onSubmit}>
+        <div className=''>
+          <label className='form-label text-white'>Name</label>
+          <input
+            type='text'
+            className='form-control text-black'
+            id='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <button type='submit' data-bs-dismiss='modal' className='bg-slate-600'>
+          Submit
+        </button>
+      </form>
+      <p className='text-white'>{other}</p>
+    </div>
+  )
 }
 
 export default SignUpList
